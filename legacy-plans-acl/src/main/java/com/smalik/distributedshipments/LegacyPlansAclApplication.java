d11bb08cc9d7917216b853cff2e41431d12b4398
Salman Malik <smalik@pivotal.io>
2019-10-31 18:20:03 -0400

    initial commit

---
+++ legacy-plans-acl/src/main/java/com/smalik/distributedshipments/LegacyPlansAclApplication.java
@@ -0,0 +1,13 @@
+package com.smalik.distributedshipments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacyPlansAclApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegacyPlansAclApplication.class, args);
+	}
+
+}
